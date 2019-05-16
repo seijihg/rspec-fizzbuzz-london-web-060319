a3db2b2706a70f3690e21dcca554cffecff9ecc3
Le H Ngo <seiji@o2.pl>
2019-05-16 20:10:25 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,7 @@
 def fizzbuzz(num)
   if num % 3 == 0 && num % 5 != 0
     "Fizz"
-  elsif num % 5 == 0
+  elsif num % 5 == 0 && num % 3 != 0
     "Buzz"
   elsif num % 3 && num % 5 == 0
     "FizzBuzz"
