b7803faaf2b187746ebd16b343e718191ebd46ce
Le H Ngo <seiji@o2.pl>
2019-05-16 20:02:34 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,10 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
   if num % 3 == 0
-    puts "Fizz"
+    "Fizz"
   elsif num % 5 == 0
-    puts "Buzz"
+    "Buzz"
   else
-    puts num
+    num
   end
 end
